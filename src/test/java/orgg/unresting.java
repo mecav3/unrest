9b102f1745012ad26de2cc2cac60620982fb22a7
mecav3 <74742815+mecav3@users.noreply.github.com>
2023-04-05 11:19:01 +0300

    unrest 1

--- src/test/java/orgg/unresting.java
+++ src/test/java/orgg/unresting.java
@@ -1,16 +1,26 @@
 package orgg;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class unresting {
-  //  @Test
-    void name() {
-        String url = "https://jsonplaceholder.typicode.com/todos/1";
+import static io.restassured.RestAssured.*;
 
-        Response response = RestAssured.given().accept("application/json").get(url);
+public class unresting {
+
+   // @Test
+    void name() {
+        baseURI    = "https://jsonplaceholder.typicode.com/todos";
+        Response response = given()
+                .accept("application/json")
+                .accept(ContentType.JSON)
+               // .pathParam("id",1)
+             //   .get("/{id}");
+                .queryParam("id","1")
+                .get( );
 
         System.out.println(response.getStatusCode());
         System.out.println(response.asPrettyString());
@@ -18,6 +28,7 @@ public class unresting {
         System.out.println(response.getContentType());
 
         System.out.println(response.body().asString());
+        System.out.println(response.headers());
 
         Assertions.assertEquals(200, response.statusCode());
 
@@ -25,16 +36,34 @@ public class unresting {
 
     }
 
-    @Test
+    //@Test
     void name01() {
-        RestAssured.baseURI = "https://official-joke-api.appspot.com/random_joke";
+        baseURI = "https://official-joke-api.appspot.com/random_joke";
 
-        Response response = RestAssured.get();
+        Response response = given().accept(ContentType.JSON).get();
+
         System.out.println(response.getStatusCode());
         System.out.println(response.contentType());
         System.out.println(response.headers().hasHeaderWithName("Date"));
         System.out.println(response.header("Date"));
         System.out.println(response.header("content-length"));
         System.out.println(response.body().asPrettyString());
+    }
+
+     @Test
+    void name3() {
+        baseURI = "https://official-joke-api.appspot.com/random_joke";
+        Response response = get();
+        System.out.println(response.asPrettyString());
+
+        String s = response.path("setup");
+        String ss = response.path("punchline");
+        System.out.println(s);
+        System.out.println(ss);
+
+        JsonPath jsonPath = response.jsonPath();
+        jsonPath.prettyPrint();
+
+
     }
 }
